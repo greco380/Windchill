852932379f7f30629ad55f3ed0ad4ee9541906b6
shanereid19 <shanereid1999@hotmail.com>
2019-09-12 23:20:53 -0500

    This is my first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+print("Welcome to the the Windchill Calculator Program!!!")
+
+def old_wc(velocity, temp):
+  """
+  [This will produced a number that represents how cold a place actually feels due to the wind using an old equation. The first people to create a formula for windchill were two Antarctic explorers in the 1940’s (Siple and Passel]
+  Args:
+  velocity: [velocity is the change in displacement over the change in time.]
+  temp: [the degree of heat present in a substance, especially as expressed according to a comparative scale and shown by a thermometer or perceived by touch.]
+  """
+  velocity = 
+
+
+def new_wc(velocity, temp):
+  """
+  [The formulas for windchill have changed over the years as scientists have refined the measure.]
+  Args:
+  velocity: [velocity is the change in displacement over the change in time.]
+  temp: [the degree of heat present in a substance, especially as expressed according to a comparative scale and shown by a thermometer or perceived by touch.]
+  """
+
+print("Enter a velocity in miles/hour.")
+velocity = (int(input())
+
+print("Enter a temperature in degrees Fahrenheit.")
+tempF = float(inupt())
+
+new_wc(velocity, tempF)
